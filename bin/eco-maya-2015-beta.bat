899091b73eb4e79ad487b50584fdbe808f53c7be
jcochrane@work <johnny@rotorstudios.com>
2015-07-08 00:27:09 +1000

    - tidy - eco title

--- bin/eco-maya-2015-beta.bat
+++ bin/eco-maya-2015-beta.bat
@@ -1,4 +1,4 @@
 @echo off&setlocal
 call %~dp0ecosystem-init.bat
-title eco-system-maya-2015-beta
+title eco-system-maya-2015-rc
 call %python_exe% %ECO_ROOT%\bin\ecosystem.py -r maya -t base,maya2015,beta,mayaPortable
