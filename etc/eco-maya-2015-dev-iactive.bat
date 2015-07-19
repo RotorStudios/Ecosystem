bd4319d50e162ceaec36b506823e48a3abb16cc0
Johnny Cochrane <johnny@rotorstudios.com>
2015-07-19 21:02:32 +1000

    - updated name from refactor
    Includes:
    - DEV_AREA >> RS_DEV_AREA
    - maya >> mayapy
    - mayaPortable >> mayaportable

--- etc/eco-maya-2015-dev-iactive.bat
+++ etc/eco-maya-2015-dev-iactive.bat
@@ -2,5 +2,4 @@
 
 title %~n1
 call %~dp0init-launcher.bat
-s
-call %python_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaPortable,dev,dept-iactive -r maya
+call %python_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,dev,dept-iactive -r maya
