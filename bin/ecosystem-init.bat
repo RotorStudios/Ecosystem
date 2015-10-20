0cfff77fee70679235a12430950f92eee5f54311
Johnny Cochrane <johnny@rotorstudios.com>
2015-10-20 23:07:39 +1100

    - using pythonw for launching aliases

--- bin/ecosystem-init.bat
+++ bin/ecosystem-init.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 
-set python_exe=S:\python\Python27\pythonw.exe
+set python_exe=S:\python\Python27\python.exe
+set pythonw_exe=S:\python\Python27\pythonw.exe
 
 for %%d in (%~dp0..) do set ECO_ROOT=%%~fd
 
@@ -10,13 +11,14 @@ set PATH=%PATH%;%ECO_ROOT%\bin
 
 Doskey eco=%python_exe% %ECO_ROOT%\bin\ecosystem.py $*
 
-Doskey maya-2015-stable=%python_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,stable -r maya
-Doskey   maya-2015-beta=%python_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,beta -r maya
-Doskey  maya-2015-alpha=%python_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,alpha -r maya
+Doskey maya-2015-stable=%pythonw_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,stable -r maya
+Doskey   maya-2015-beta=%pythonw_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,beta -r maya
+Doskey  maya-2015-alpha=%pythonw_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,alpha -r maya
 
-Doskey    maya-2015-dev=%python_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,dev -r maya
-Doskey maya-2015-dev-jc=%python_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,devjc -r maya
+Doskey    maya-2015-dev=%pythonw_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,dev -r maya
+Doskey maya-2015-dev-jc=%pythonw_exe% %ECO_ROOT%\bin\ecosystem.py -t base,maya2015,mayaportable,devjc -r maya
 
-Doskey     eclipse-luna=start %python_exe% %ECO_ROOT%\bin\ecosystem.py -t base,eclipse-luna -r eclipse-luna-setup.bat
+Doskey eclipse-luna=start %python_exe% %ECO_ROOT%\bin\ecosystem.py -t base,eclipse-luna -r eclipse-luna-setup.bat
+
 
 Doskey     rv="S:\dev_area\apps\tweak\rv-stable.lnk"
